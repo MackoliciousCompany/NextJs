48590cc5569f8480aa6df7d80df6b6952ca2df50
Lennox M <lennox@mackolicious.com>
2020-10-28 10:36:13 +0000

    Added linting

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
@@ -8,7 +9,8 @@ export default function FirstPost() {
           <Head>
         <title>First Post</title>
       </Head>
-      <h1>First Post</h1>
+      <h1> 
+      First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
