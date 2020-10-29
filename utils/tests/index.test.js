54a51516f56d715a7c1c7498a749ec921987c973
Lennox M <mackolicious@hotmail.com>
2020-10-29 21:25:48 +0000

    Updated index.test.js

--- utils/tests/index.test.js
+++ utils/tests/index.test.js
@@ -1,6 +1,6 @@
 import { util } from '..'
 
 test('four plus nine is thirteen', () => {
-    // expect(util(4)).toBe(13);
-    expect(6).toBe(6);
+    expect(util(4)).toBe(13);
+    // expect(6).toBe(6);
 });
